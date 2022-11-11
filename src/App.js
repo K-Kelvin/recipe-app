5e30d62c52e47d8c995a6fa3b86c77c9ce3c21bf
Kelvin Kijanda <kelvinkijanda@gmail.com>
2022-11-11 20:31:33 +0000

    Initial commit, no extra packages added to package.json

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import GlobalStyle from "./styles/globalStyles";
+import { ThemeProvider } from "styled-components";
+import { lightTheme } from "./styles/theme";
+import AuthProvider from "./context/UserContext";
+import MainRoutes from "./routes";
+
+function App() {
+  const themeStyles = lightTheme;
+  
+  return (
+    <ThemeProvider theme={themeStyles}>
+      <div className="App">
+        <GlobalStyle />
+
+        <Router>
+          <AuthProvider>
+            <MainRoutes />
+          </AuthProvider>
+        </Router>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/pages/diet/Diet.tsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import Navbar from "../../components/Navbar";
+import CustomLoader from "../../components/CustomLoader";
+import styled from "styled-components";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useQuery } from "react-query";
+import MealDiet from "./MealDiet";
+
+interface Props {
+  isLoading?: boolean;
+  diet?: any;
+  nutrients?: any[];
+}
+const Diet = (props: Props) => {
+  const { id } = useParams();
+
+  const fetchDiet = async () => {
+    const response = await axios(
+      `https://api.spoonacular.com/mealplanner/generate?apiKey=${process.env.REACT_APP_API_KEY}&timeFrame=day&diet=${id}`
+    );
+    return response;
+  };
+  const { data, isLoading } = useQuery(["fetch-diet", id], fetchDiet);
+  console.log(data, "data diet component");
+  let objectClone = {};
+  objectClone = { ...data?.data?.meals, ...data?.data?.nutrients };
+  console.log(objectClone, "object");
+
+  return (
+    <>
+      <Navbar />
+      {
+        <>
+          {isLoading ? (
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "30px",
+              }}
+            >
+              <CustomLoader />
+            </div>
+          ) : (
+            <>
+              <div>
+                {data?.data?.nutrients &&
+                  Object.entries(data?.data?.nutrients)?.map((item: any) => (
+                    <div>
+                      <p>{item[0]}</p>
+                      <p>{item[1].toFixed(0)}</p>
+                    </div>
+                  ))}
+              </div>
+              <div>
+                {data?.data?.meals.map((meal: any) => (
+                  <MealDiet key={meal.id} meal={meal} />
+                ))}
+              </div>
+            </>
+          )}
+        </>
+      }
+    </>
+  );
+};
+
+export default Diet;
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-gap: 3rem;
+`;
+const Box = styled.div`
+  padding: 10px 20px;
+  height: 300px;
+  width: 400px;A
+  margin: 12px auto;
+  border-radius: 10px;
+  overflow: hidden;
+
+  img {
+    border-radius: 10px;
+  }
+`;
